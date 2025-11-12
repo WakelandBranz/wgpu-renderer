5c6407673bf193adcec12e82f5b1acd0df479ce5
wakelandbranz <wbranz@icloud.com>
2025-11-12 00:06:28 -0500

    Removed vestigial file

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 pub mod camera;
 pub mod error;
 pub mod renderer;
-pub mod sprite;
 pub mod types;
 
 pub use camera::Camera2D;
 pub use error::RenderError;
 pub use renderer::Renderer;
-pub use sprite::Sprite;
 pub use types::*;
